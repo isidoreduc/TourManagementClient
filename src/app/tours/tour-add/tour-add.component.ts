285608d8294908d6784f8e59c24a5eb35c63cdda
isidoreduc <isidoreducasse79@gmail.com>
2019-04-25 14:59:05 +0200

    extra

--- src/app/tours/tour-add/tour-add.component.ts
+++ src/app/tours/tour-add/tour-add.component.ts
@@ -16,7 +16,7 @@ export class TourAddComponent implements OnInit {
   public tourForm: FormGroup;
   bands: Band[];
   managers: Manager[];
-  private isAdmin = true;
+  private isAdmin = false;
 
   constructor(
     private masterDataService: MasterDataService,
