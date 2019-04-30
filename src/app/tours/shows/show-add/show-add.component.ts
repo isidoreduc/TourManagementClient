122f271fe721e596cd63770805b9b987d4025abe
isidoreduc <isidoreducasse79@gmail.com>
2019-04-30 12:34:49 +0200

    update

--- src/app/tours/shows/show-add/show-add.component.ts
+++ src/app/tours/shows/show-add/show-add.component.ts
@@ -1,13 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { ShowSingleComponent } from '../show-single/show-single.component';
+import { ShowService } from '../shared/show.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-show-add',
   templateUrl: './show-add.component.html',
   styleUrls: ['./show-add.component.css']
 })
+export class ShowAddComponent implements OnInit {
+  private sub: Subscription;
+  private tourId: string;
+  public showCollectionForm: FormGroup;
 
-export class ShowAddComponent implements OnInit { 
+  constructor(
+    private showService: ShowService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.showCollectionForm = this.formBuilder.group({
+      shows: this.formBuilder.array([])
+    });
+
+    this.addShow();
+
+    // get route data (tourId)
+    this.sub = this.route.params.subscribe(params => {
+      this.tourId = params['tourId'];
+    });
+  }
+
+  addShow(): void {
+    const showsFormArray = this.showCollectionForm.get('shows') as FormArray;
+    showsFormArray.push(ShowSingleComponent.createShow());
+  }
+
+  addShows(): void {
+    if (this.showCollectionForm.dirty && this.showCollectionForm.value.shows.length) {
+      const showCollection = automapper.map(
+        'ShowCollectionFormModelShowsArray',
+        'ShowCollectionForCreation',
+        this.showCollectionForm.value.shows
+      );
+
+      this.showService.addShowCollection(this.tourId, showCollection).subscribe(() => {
+        this.router.navigateByUrl('/tours');
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
